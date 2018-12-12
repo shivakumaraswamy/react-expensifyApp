31565a3b125f8571a891b6297fcfe4d747959726
Swamy <Shivakumara.Swamy@tui.co.uk>
2018-12-12 20:53:48 +0000

    Changed date and currency format

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,50 @@
 const path = require("path");
+const MiniCssExtractPlugin  = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== 'production'
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        loader: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/
-      },
-      {
-        test: /\.css$/,
-        include: /node_modules/,
-        loaders: ["style-loader", "css-loader"]
-      }
-    ]
-  },
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true
-  }
+module.exports = () => {
+  
+  return {
+    mode: 'production',
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          loader: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: [
+            {   
+              loader: MiniCssExtractPlugin.loader,
+              options: {
+                // you can specify a publicPath here
+                // by default it use publicPath in webpackOptions.output
+                publicPath: '../'
+              }
+            },
+            "css-loader"
+          ]
+        }
+      ]
+    },
+    plugins:[
+      new MiniCssExtractPlugin({
+        // Options similar to the same options in webpackOptions.output
+        // both options are optional
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      })
+    ],
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true
+    }
+  };
 };
